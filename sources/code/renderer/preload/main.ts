1c587a9300e647f41464fa28c68683cef3bded93
LandonSiler <paypal.silerlandon@gmail.com>
2026-01-05 02:11:20 -0500

    fix: Make link and image elements drag safe for linux

--- sources/code/renderer/preload/main.ts
+++ sources/code/renderer/preload/main.ts
@@ -42,7 +42,7 @@ if (window.location.protocol === "file:") {
     document.addEventListener("paste", (event) => {
       const contentTypes = clipboard.availableFormats() as []|[string, string];
       if(contentTypes.length === 2 && contentTypes[0].startsWith("image/") &&
-          contentTypes[1] === "text/html" && lock) {
+        contentTypes[1] === "text/html" && lock) {
         console.debug("[WebCord] Applying clipboard workaround to the image…");
         lock = false;
         // Electron will somehow sort the clipboard to parse it correctly.
@@ -58,6 +58,24 @@ if (window.location.protocol === "file:") {
       lock = true;
       return;
     }, true);
+  }
+
+  /**
+   * The following is an attempt to fix a known state issue with drag and drop inside electron on linux.
+   * When dragging images or links, the dragstart will fire as expected; 
+   * but in cases where the drop occurs inside the window bounds on an element that is not a valid drop target
+   * it appears that the dragend event does not fire causing a stuck state of constant dragging.
+   * TODO: Remove this workaround when the issue is fixed in Electron.
+   */
+  if (process.platform === "linux") {
+    console.debug("[WebCord] Applying drag/drop workaround for Linux…");
+    document.addEventListener('dragstart', (e) => {
+      if (e.target === null) return;
+      if (!(e.target instanceof HTMLElement)) return;
+      if (e.target.nodeName === "IMG" || e.target.nodeName === "A") {
+        e.preventDefault();
+      }
+    });
   }
 
   /*
